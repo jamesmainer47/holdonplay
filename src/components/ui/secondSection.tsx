9c1b0a063baa8b724f7eb77091e289d14d513ba9
StupidBoyfriend <118992198+jamesmainer47@users.noreply.github.com>
2025-07-24 21:17:21 +0300

    Update secondSection.tsx

--- src/components/ui/secondSection.tsx
+++ src/components/ui/secondSection.tsx
@@ -8,7 +8,7 @@ export default function SecondSection(){
         <div className="bg-black flex flex-col justify-center items-center p-20 text-center text-2xl ">
             <div className="w-100 md:w-200 sm:w-150 lg:w-350">
             <h1 className="lg:text-6xl font-bold text-2xl text-blue-500">Make your Music popular Worldwide</h1>
-            <p className="text-xl sm:text-sm ">Increase the reach of your music across the most popular stores & platforms like Spotify, Apple Music, TikTok, Youtube and many more. Empower yourself with unlimited distribution opportunities, and get your music heard by a global audience</p>
+            <p className="text-xl sm:text-sm text-blue-100">Increase the reach of your music across the most popular stores & platforms like Spotify, Apple Music, TikTok, Youtube and many more. Empower yourself with unlimited distribution opportunities, and get your music heard by a global audience</p>
             <p>Keep 100% ownership of your music, maintaining creative control and authority in your music career</p>
             <div className="flex self-center items-center justify-center p-4">{logoz.map((src, i) => (
                 <Image src={src} key={src} width={200} height={300} alt="logos" className="size-20 lg:size-40 "/>
